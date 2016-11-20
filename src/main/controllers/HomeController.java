cb9b3eb853a3c1a923bb7c59d3ce6b95421ad4fb
Daniel Eliasinski <danieleliasinski@msn.com>
2016-11-20 01:33:47 -0500

    Fixed font bug.

--- src/main/controllers/HomeController.java
+++ src/main/controllers/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController extends RuntimeException {
+public class HomeController{
 	
 	@RequestMapping(value = "/")
     public String home() {
