6e16791282e08a39805368c97471c94c93ea584d
Daniel Eliasinski <danieleliasinski@msn.com>
2016-11-23 01:50:48 -0500

    Simplified error page handling. No java code is needed just put pages in correct places with correct names and spring boot will use them.

--- src/main/Application.java
+++ src/main/Application.java
@@ -2,33 +2,11 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class Application  {
 	
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
-	
-	private static final String PATH = "/errors";
-	
-	@Bean
-	public EmbeddedServletContainerCustomizer containerCustomizer(){
-		return (container -> {
-			ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, PATH);
-			ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, PATH);
-			ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, PATH);
-			ErrorPage allErrorPage = new ErrorPage(PATH);
-			container.addErrorPages(error404Page,error500Page, error400Page, allErrorPage);
-			
-		});
-
-	}
-
-
 }
