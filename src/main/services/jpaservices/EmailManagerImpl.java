3eac73b87e5ae799ba58ba1c7cfb7ac3b4b38538
Daniel Eliasinski <danieleliasinski@msn.com>
2016-11-24 20:36:15 -0500

    Finished email verfication code and created htmlreader method with injectable objects. Later we can code nice looking emails with it using html.

--- src/main/services/jpaservices/EmailManagerImpl.java
+++ src/main/services/jpaservices/EmailManagerImpl.java
@@ -1,15 +1,16 @@
 package main.services.jpaservices;
 
-import java.util.Date;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Scanner;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import main.modelpojos.Idea;
 import main.services.serviceinterfaces.EmailManager;
@@ -17,31 +18,84 @@ import main.services.serviceinterfaces.EmailManager;
 /**
  * Created by Armando on 11/23/2016.
  */
-@Service
-public class EmailManagerImpl implements EmailManager{
+@Component
+public class EmailManagerImpl implements EmailManager {
+	
+	@Value("${spring.mail.username}")
+	private String username;
+	@Value("${spring.mail.password}")
+	private String password;
+	@Value("${spring.mail.port}")
+	private int port;
 
-    private JavaMailSender mailSender;
-
-    @Autowired
-    public void EmailVerificationManager(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-    
-    public void verifyEmail(Idea idea) throws MailException{
-        String body = "You have sumbitted an idea to (website name here). <br/> <a href=\"http://localhost:8080/ideas/" + idea.getId() + "/" + idea.getVerificationLink() + "\">Click here to verify and display your idea!</a>";
-        MimeMessage mailMessage = mailSender.createMimeMessage();
-        try {
-			mailMessage.addRecipients(Message.RecipientType.TO, idea.getEmail());
-			mailMessage.setFrom("sbuwebappclub@gmail.com");
-	        mailMessage.setSubject("Verify your Idea");
-	        mailMessage.setText(body, "UTF-8", "html");
-	        mailMessage.setSentDate(new Date());
-		} catch (MessagingException e) {
+	public void verifyEmail(Idea idea){
+		// String body = "You have sumbitted an idea to (website name here).
+		// <br/> <a href=\"http://localhost:8080/ideas/" + idea.getId() + "/" +
+		// idea.getVerificationLink() + "\">Click here to verify and display
+		// your idea!</a>";
+		// MimeMessage mailMessage = mailSender.createMimeMessage();
+		// try {
+		// mailMessage.addRecipients(Message.RecipientType.TO, idea.getEmail());
+		// mailMessage.setFrom("sbuwebappclub@gmail.com");
+		// mailMessage.setSubject("Verify your Idea");
+		// mailMessage.setText(body, "UTF-8", "html");
+		// mailMessage.setSentDate(new Date());
+		// } catch (MessagingException e) {
+		// e.printStackTrace();
+		// }
+		// Runnable run = () -> mailSender.send(mailMessage);
+		// Thread thread = new Thread(run);
+		// thread.setName("Email Sending To " + idea.getEmail());
+		// thread.start();
+		HtmlEmail email = new HtmlEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(port);
+		email.setAuthenticator(new DefaultAuthenticator(username, password));
+		email.setSSLOnConnect(true);
+		try {
+			email.setFrom("sbuwebappclub@gmail.com");
+			email.setSubject("TestEmail");
+			email.setMsg("Test Message from Armando Xhimanki. ");
+			email.addTo(idea.getEmail());
+			// image
+			StringBuffer msg = new StringBuffer();
+			msg.append("<html><body>");
+			msg.append("<p>You have sumbitted an idea to (website name here).</p>");
+			msg.append("<a href = http://localhost:8080/idea/").append(idea.getId()).append("/")
+					.append(idea.getVerificationLink()).append(">");
+			msg.append("Click here to verify your email").append("</a>");
+			// email.setMsg(msg.toString());
+			email.setHtmlMsg(msg.toString());
+			email.setSubject("Validate");
+			// fallback
+			email.setTextMsg("Your email client does not support html");
+			Runnable run = () -> {
+				try {
+					email.send();
+				} catch (EmailException e) {
+					e.printStackTrace();
+				}
+			};
+			Thread thread = new Thread(run);
+			thread.setName("Email Sending To " + idea.getEmail());
+			thread.start();
+		} catch (EmailException e) {
 			e.printStackTrace();
 		}
-        Runnable run = () -> mailSender.send(mailMessage);
-        Thread thread = new Thread(run);
-        thread.setName("Email Sending To " + idea.getEmail());
-        thread.start();
-    }
+	}
+	
+	private String readTextFile(String aFileName, HashMap<String, String> mappings) throws IOException {
+		Path path = Paths.get(aFileName);
+		String s = "";
+		try (Scanner scanner = new Scanner(path)) {
+			while (scanner.hasNextLine()) {
+				s += scanner.nextLine() + "\n";
+			}
+			scanner.close();
+		}
+		for(String keys : mappings.keySet()){
+			s.replace(keys, mappings.get(keys));
+		}
+		return s;
+	}
 }
