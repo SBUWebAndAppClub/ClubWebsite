81e1791aa740962faf419c388189773b943644e3
Hid <mikeygulati@gmail.com>
2016-12-09 15:54:45 -0500

    added 404, general error page, and verified email success html/css

--- src/main/controllers/HomeController.java
+++ src/main/controllers/HomeController.java
@@ -12,16 +12,16 @@ import main.services.serviceinterfaces.MemberService;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private MemberService memberService;
 
-	@RequestMapping(value = "/")
-	public String home(Model model) {
-		List<Member> members = memberService.getMembers();
-		model.addAttribute("member1", members.get(0));
-		model.addAttribute("member2", members.get(1));
-		model.addAttribute("member3", members.get(2));
-		return "index";
-	}
+    @Autowired
+    private MemberService memberService;
+
+    @RequestMapping(value = "/")
+    public String home(Model model) {
+        List<Member> members = memberService.getMembers();
+        model.addAttribute("member1", members.get(0));
+        model.addAttribute("member2", members.get(1));
+        model.addAttribute("member3", members.get(2));
+        return "index";
+    }
 }
