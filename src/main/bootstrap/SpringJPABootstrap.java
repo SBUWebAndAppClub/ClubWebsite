c0b958dae7c5476de868eec03f8158069c5dc7a5
Ravjot Sachdev <Ravjot Sachdev>
2017-03-08 14:14:39 -0500

    Added member info

--- src/main/bootstrap/SpringJPABootstrap.java
+++ src/main/bootstrap/SpringJPABootstrap.java
@@ -50,9 +50,14 @@ public class SpringJPABootstrap  implements ApplicationListener<ContextRefreshed
     	Random r = new Random();
     	
         //Default image path: http://i.imgur.com/QUOGtiN.png
+        //Urls array order {Facebook, Google Plus, Github}
+        //Replace ' ' in a URL with '%20'
+        //Format for URLS: "URL NameToBeShown"
+            //Example: "github.com Github"
+        //nulls in setUrls causes individual member pages to break  <---------------FIX THIS in member.html
 
         Member m1 = new Member();
-        m1.setId(r.nextInt(Integer.MAX_VALUE));
+        m1.setId(r.nextInt());
         m1.setFullName("Armando Xhimanki");
         m1.setEmail("armando.xhimanki@stonybrook.edu");
         m1.setMajor("Computer Science");
@@ -62,66 +67,78 @@ public class SpringJPABootstrap  implements ApplicationListener<ContextRefreshed
         m1.setProjects(new HashSet<Project>());
 
         Member m2 = new Member();
-        m2.setId(r.nextInt(Integer.MAX_VALUE));
+        m2.setId(r.nextInt());
         m2.setFullName("Mikey Gulati");
         m2.setEmail("mankirat.gulati@stonybrook.edu");
         m2.setMajor("Computer Science");
         m2.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m2.setUrls(null);
+        m2.setUrls(new String[]{null, null, "https://github.com/devhid Github"});
         m2.setBio("Vice President");
         m2.setProjects(new HashSet<Project>());
 
         Member m3 = new Member();
-        m3.setId(r.nextInt(Integer.MAX_VALUE));
+        m3.setId(r.nextInt());
         m3.setFullName("Stanley Lim");
         m3.setEmail("stanley.lim@stonybrook.edu");
         m3.setMajor("Computer Science");
         m3.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m3.getUrls();
+        m3.setUrls(new String[]{null, null, "https://github.com/Spiderpig86 Github"});
         m3.setBio("Treasurer");
         m3.setProjects(new HashSet<Project>());
 
         Member m4 = new Member();
-        m4.setId(r.nextInt(Integer.MAX_VALUE));
+        m4.setId(r.nextInt());
         m4.setFullName("Daniel Eliasinski");
         m4.setEmail("daniel.eliasinski@stonybrook.edu");
         m4.setMajor("Computer Science");
         m4.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m4.setUrls(null);
+        m4.setUrls(new String[]{null, null, "https://github.com/Mr-Slurpy Github"});
         m4.setBio("Lowly Peasant");
         m4.setProjects(new HashSet<Project>());
 
         Member m5 = new Member();
-        m5.setId(r.nextInt(Integer.MAX_VALUE));
+        m5.setId(r.nextInt());
         m5.setFullName("Ravjot Sachdev");
+        m5.setEmail("ravjot.sachdev@stonybrook.edu");
         m5.setMajor("Computer Science");
         m5.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m5.setUrls(null);
+        m5.setUrls(new String[]{null, null, "https://github.com/rsachdev19 Github"});
         m5.setBio("Lowly Peasant");
         m5.setProjects(new HashSet<Project>());
 
         Member m6 = new Member();
-        m6.setId(r.nextInt(Integer.MAX_VALUE));
+        m6.setId(r.nextInt());
         m6.setFullName("Eduardo Quispe");
         m6.setEmail("eduardo.quispe@stonybrook.edu");
-        m6.setMajor("Copmuter Science");
+        m6.setMajor("Computer Science");
         m6.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m6.setUrls(null);
+        m6.setUrls(new String[]{null, null, "https://github.com/edqu3 Github"});
         m6.setBio("Lowly Peasant");
         m6.setProjects(new HashSet<Project>());
 
         Member m7 = new Member();
-        m7.setId(r.nextInt(Integer.MAX_VALUE));
+        m7.setId(r.nextInt());
         m7.setFullName("Murshid Azher");
-        m7.setEmail("");
+        m7.setEmail("mohamedmurshid.mohamedazher@stonybrook.edu");
+        m7.setMajor("Computer Science");
         m7.setImagePath("http://i.imgur.com/QUOGtiN.png");
-        m7.setUrls(null);
+        m7.setUrls(new String[]{null, null, "https://github.com/murshidazher Github"});
         m7.setBio("Lowly Peasant");
         m7.setProjects(new HashSet<Project>());
 
+        Member m8 = new Member();
+        m8.setId(r.nextInt());
+        m8.setFullName("Steven Chou");
+        m8.setEmail("yu-hsiang.chou@stonybrook.edu");
+        m8.setMajor("Computer Science");
+        m8.setImagePath("http://i.imgur.com/QUOGtiN.png");
+        m8.setUrls(new String[]{null, null, "https://github.com/StevenChou888 Github"});
+        m8.setBio("Lowly Peasant");
+        m8.setProjects(new HashSet<Project>());
+
 
         Project p1 = new Project();
-        p1.setId(r.nextInt(Integer.MAX_VALUE));
+        p1.setId(r.nextInt());
         p1.setName("Club Website");
         p1.setDescription("It's a website.");
         p1.setImagePath("http://placehold.it/750x650");
@@ -134,6 +151,7 @@ public class SpringJPABootstrap  implements ApplicationListener<ContextRefreshed
         memberService.createMember(m5);
         memberService.createMember(m6);
         memberService.createMember(m7);
+        memberService.createMember(m8);
         projectService.createProject(p1);
 
 
@@ -144,6 +162,7 @@ public class SpringJPABootstrap  implements ApplicationListener<ContextRefreshed
         relationManager.createMemberToProjectRelation(m5.getId(), p1.getId());
         relationManager.createMemberToProjectRelation(m6.getId(), p1.getId());
         relationManager.createMemberToProjectRelation(m7.getId(), p1.getId());
-    }
+        relationManager.createMemberToProjectRelation(m8.getId(), p1.getId());
+}
 }
 
